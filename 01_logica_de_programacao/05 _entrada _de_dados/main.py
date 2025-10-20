7f0577cfc891e2cd90b991897f9284b8acf50229
Pedrobsb-git <ppedrobsb@gmail.com>
2025-10-20 17:37:50 -0300

    Aula do dia 20/10/2025.

---
+++ 01_logica_de_programacao/05 _entrada _de_dados/main.py
@@ -0,0 +1,7 @@
+# entrada de dados
+nome = input("Informe seu Nome: ")
+idade = input("Informe sua idade: ")
+escola = input("Informe aonde estuda: ")
+
+# saida de dados
+print(f"Boa tarde, meu nome é {nome:}, tenho {idade:} anos e estudo em {escola:}.")
