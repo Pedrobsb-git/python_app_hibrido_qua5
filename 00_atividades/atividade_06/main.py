c51d476f1d0d3041f05766d271d2512d79264a6b
Pedrobsb-git <ppedrobsb@gamail.com>
2025-10-30 17:52:04 -0300

    Aula 30/10/2025

---
+++ 00_atividades/atividade_06/main.py
@@ -0,0 +1,36 @@
+# declaração de dicionário
+usuario = {
+    'nome': "Marcos Silva",
+    'email': "marcossil@gmail.com",
+    'telefone': "(61)98453-2005",
+    'cpf': "321523784-01",
+    'genero': "Masculino"
+}
+
+# exibir os dados
+for chave in usuario:
+    print(f"{chave.capitalize()}: {usuario[chave]}")
+
+# usuário escolhe o campo que deseja mudar
+while True:
+    campo = input("Informe o nome do campo que deseja alterar ou digite 'sair' para encerrar o programa: ").strip().lower()
+
+    match campo:
+        case "nome":
+            usuario['nome'] = input("Informe o novo 'nome': ").strip()
+        case "email":
+            usuario['email'] = input("Informe o novo 'email': ").strip()
+        case "telefone":
+            usuario['telefone'] = int(input("Informe o novo 'telefone': ").strip())
+        case "cpf":
+            usuario['cpf'] = input("Informe o novo 'cpf': ").strip()
+        case "genero":
+            usuario['genero'] = input("Informe o novo 'genero': ").strip()
+        case "sair":
+            break
+        case _:
+            print("Valor Inválido.")
+            continue
+# mostra na tele os novos valore
+for chave in usuario:
+    print(f"{chave.capitalize()}: {usuario[chave]}")
