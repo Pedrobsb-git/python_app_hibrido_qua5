915c46f0cbbfaef863f03038c2a23899b3b60848
Pedrobsb-git <ppedrobsb@gmail.com>
2025-11-07 17:04:55 -0300

    Aula 07/11/2025

---
+++ 00_atividades/atividade_07/main.py
@@ -0,0 +1,5 @@
+# função principal
+def main():
+    # entrada de dados
+    nome = input("Informe seu Nome: ").strip().title()
+    idade = int(input("Informe sua Idade: ").strip())
--- 06_modulo_parte_02/main.py
+++ 06_modulo_parte_02/main.py
@@ -1,15 +1,4 @@
-# função principal
-def main():
-    # entrada de dados
-    nome = input("Informe seu Nome: ").strip().title()
-    idade = int(input("Informe sua Idade: ").strip())
-
-    # operador ternário
-    resultado = "é maior de idade." if idade >= 18 else "é menor de idade"
-
-    # saída de dados
-    print(f"{nome} {resultado}")
-
-# protege algoritimo principal
-if __name__ == "__main__":
- main()
+# classes
+class Conta:
+    def __init__(self, titular, cpf, saldo):
+        pass
