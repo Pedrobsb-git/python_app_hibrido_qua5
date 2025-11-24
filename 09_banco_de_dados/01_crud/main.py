d8c113482c376729de925242094a1170ac549c2d
Pedrobsb-git <ppedrobsb@gmail.com>
2025-11-24 17:24:46 -0300

    Aula 24/11/2025

--- 09_banco_de_dados/01_crud/main.py
+++ 09_banco_de_dados/01_crud/main.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 from entidades import criar_tb_pessoa
-from modulo import limpar
+from modulo import limpar, cadastrar, listar, atualizar, deletar
 
 def main():
     enegine = create_engine("sqlite:///01_crud/database/crud.db")
@@ -12,9 +12,34 @@ def main():
     session = Session()
 
     limpar()
-    # TODO: fazer o CRUD
-
-    session.close()
-
-if __name__ == "__main__":
-    main()
+    while True:
+        print(f"{'-'*20} 🍎 CRUD DA COBRA 🍎 {'-'*20}\n")
+        print("0 - Sair do programa")
+        print("1 - Cadastrar nova pessoa")
+        print("2 - Listar pessoas")
+        print("3 - Atualizar Dados")
+        print("4 - Excluir pessoa")
+        opcao = input("Opção desejada: ").strip()
+        limpar()
+        match opcao:
+                case "0":
+                    print("Programa encerrado.")
+                    break
+                case "1":
+                    print(cadastrar(session, Pessoa))
+                    continue
+                case "2":
+                    listar(session, Pessoa)
+                    continue
+                case "3":
+                    print(atualizar(session, Pessoa))
+                    continue
+                case "4":
+                    print(deletar(session, Pessoa))
+                    continue
+                case _:
+                    print("Opção Invalida.")
+                    continue
+        session.close()
+    if __name__ == "__main__":
+            main()
--- 09_banco_de_dados/01_crud/modulo.py
+++ 09_banco_de_dados/01_crud/modulo.py
@@ -1,4 +1,159 @@
 import os
+from datetime import datetime
 
 def limpar():
     os.system("cls" if os.name == "nt" else "clear")
+    
+
+def cadastrar(session, Pessoa):
+    try:
+        nome = input("Informe o Nome: ").strip().title()
+        genero = input("Informe seu Gênero: ").strip()
+        nascimento = input("Informe sua data de nascimento (dd/mm/aaaa): ").strip
+        nascimento = datetime.strptime(nascimento, "%d/%m/%Y").date()
+        email = input("Informe o seu email: ").strip().lower()
+       
+        pessoas =  session.query(Pessoa).filter(Pessoa.email.like(email)).all()
+        
+        if email in [pessoa.email for pessoa in pessoas]:
+            return "E-mail já cadastrado."
+
+        else:    
+            nova_pessoa = Pessoa (
+                nome=nome,
+                nascimento=nascimento,
+                email=email,
+                genero=genero
+            )
+
+            # insert into pessoa
+            session.add(nova_pessoa)
+            session.commit()
+
+            return f" {nova_pessoa.nome} Cadastrada com Sucesso!"
+
+    except Exception as e:
+        print(f"Não foi possivel cadastrar. {e}.") 
+        
+def listar(session, Pessoa):
+    try:
+        pessoas = session.query(Pessoa).all()
+        
+        print("Pessoas cadastradas:\n")
+        for pessoa in pessoas:
+            print(f"ID: {pessoa.id_pessoa}")
+            print(f"Nome: {pessoa.nome}")
+            print(f"E-mail: {pessoa.email}")
+            print(f"Gênero: {pessoa.genero}")
+            print(f"Data de Nascimento: {pessoa.nascimento.strftime("%d/%m/%Y")}")
+            print(f"\n{'-'*40}\n")
+    except Exception as e:
+        print(f"Não foi possivel listar. {e}.")
+        
+#update
+def atualizar(session, Pessoa):
+    id_pessoa = ""
+    email = ""
+    novo_nome = ""
+    novo_email = ""
+    novo_nascimento = ""
+    novo_genero = ""
+    
+    try:
+        print("Escolha o campo que deseja pesquisar: ")
+        print("1 - ID")
+        print("2 - E-mail")
+        print("3 - Retornar")
+        opcao = input("Opção desejada: ").strip()
+        limpar()
+        match opcao:
+            case "1":
+                id_pessoa = input("Informe o ID: ").strip
+                pessoa = session.query(Pessoa).filter_by(id_pessoa=id_pessoa).first()
+            case "2":
+                email = input("Informe o e-mail: ").strip().lower()
+                pessoa = session.query(Pessoa).filter_by(email=email).first()
+            case "3":
+                return ""
+            case _:
+                return "Opção invalida."
+        
+        if pessoa:
+            limpar()
+            while True:
+                print(f"ID {pessoa.id_pessoa}")    
+                print("Qual campo deseja alterar:\n")
+                print(f"1 - Nome: {pessoa.nome}")
+                print(f"2 - E-mail: {pessoa.email}")
+                print(f"3 - Data de nascimento: {pessoa.nascimento.strftime("%d/%m/%Y)}")
+                print(f"4 - Gênero: {pessoa.genero}")
+                print(f"5 - Finalizar")
+                campo = input("Campo desejado: ").strip()
+                limpar()
+                  match campo:
+                        case "1":
+                            novo_nome = input("Informe o Novo Nome: ").strip().title()
+                        case "2":
+                            novo_email = input("Informe o Novo E-mail: ").strip().lower()
+                            pessoas = session.query(Pessoa).filter(Pessoa.email == novo_email).all()
+                            if email in [pessoa.email for pessoa in pessoas]:
+                                print("E-mail já cadastrado!")
+                        case "3":
+                            novo_nascimento =  input("Informe a nova data de nascimento (dd/mm/aaaa): ").strip()
+                        case "4":
+                            novo_genero = input("Informe o novo gênero: ").strip().lower()
+                            continue
+                        case "5":
+                            novo_nome = novo_nome if novo_nome != "" else pessoa.nome
+                            novo_email = novo_email if novo_email != "" else pessoa.email
+                            novo_nascimento = datetime.strptime(novo_nascimento,"%d/%m/%Y").date() if novo_nascimeto != "" else pessoa.nascimento
+                            novo_genero = novo_genero if novo_genero != "" else pessoa.genero
+
+        pessoa.nome
+    except Exception as e:
+        print(f"Não foi possivel alterar os dados. {e}.")
+        
+def deletar(session, Pessoa):
+        id_pessoa = "" 
+        email = ""
+        pessoa = ""
+        
+print("Informe o campo que deseja pesquisar: ")
+print("1 - ID")
+print("2 - E-mail")
+print("3 - Retornar")
+opcao = input("Informe o campo que deseja pesquisar: ").strip
+limpar()
+    match opcao:
+        case "1":
+            id_pessoa = input("Informe o ID a ser excluido: ").strip()
+            pessoa = session.query(Pessoa).filter_by(id_pessoa=id_pessoa).first()
+        case "2":
+            email = input("Iforme o E-mail do cadastro a ser excluido: ").strip().lower()
+            pessoa = session.query(Pessoa).filter_by(email=email).first()
+        case "3"
+            return ""
+        case _:
+            return "Opção inválida."
+
+if pessoa:
+    limpar()
+    print(f"ID: {pessoa.id_pessoa}")
+    print(f"Nome: {pessoa.nome}")
+    print(f"E-mail: {pessoa.email}")
+    print(f"Gênero: {pessoa.genero}")
+    print(f"Data de Nascimento: {pessoa.nascimento.strftime("%d/%m/%Y")}")
+    print({'-'*40})
+    print("1 - Sim")
+    print("2 - Não")
+    excluir = intput("Tem certeza de que deseja excluir o registro? ").strip()
+    match excluir:
+        case "1":
+            session.delete(pessoa)
+            session.commit()
+            return "Registro excluído com sucesso"
+        case "2":
+            return ""
+        case_:
+            return "Opção Inválida."
+            
