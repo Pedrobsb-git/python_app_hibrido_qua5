1670bca5a6d79bc6b4b4c0170ee1dd353799e4b2
Pedrobsb-git <ppedrobsb@gmail.com>
2025-12-05 17:27:21 -0300

    Aula do dia 05/12/2025.

--- 01_logica_de_programacao/00_anotacao/main.py
+++ 01_logica_de_programacao/00_anotacao/main.py
@@ -1,6 +1,78 @@
-git config git user.name Pedrobsb-git
- git
-git config user.name Pedrobsb-git
-git config user.email ppedrobsb@gmail.com
- git add .
- git status
+estoque = {}
+
+while True:
+    print("""
+========= SISTEMA DE CONTROLE DE ESTOQUE SENAI - TAGUATINGA DF =========
+1 - Adicionar um item
+2 - Adicionar preço
+3 - Remover um item
+4 - Atualizar quantidade
+5 - Listar estoque
+6 - Sair
+""")
+
+    opcao = input("Escolha uma opção,por gentileza!: ")
+
+    # ----------------- ADICIONAR ITEM -----------------
+    if opcao == "1":
+        nome = input("Nome do item: ").lower()
+        quantidade = int(input("Quantidade: "))
+
+        if nome in estoque:
+            estoque[nome]["quantidade"] += quantidade
+        else:
+            estoque[nome] = {"quantidade": quantidade, "preco": 0.0}
+
+        print(f"✔ Item '{nome}' adicionado/atualizado com sucesso!\n")
+
+    # ----------------- ADICIONAR PREÇO -----------------
+    elif opcao == "2":
+        nome = input("Nome do item para adicionar preço: ").lower()
+
+        if nome in estoque:
+            preco = float(input("Digite o preço do item (R$): "))
+            estoque[nome]["preco"] = preco
+            print(f"✔💵 Preço do item '{nome}' atualizado com sucesso!\n")
+        else:
+            print("❌ Item não encontrado.\n")
+
+    # ----------------- REMOVER ITEM -----------------
+    elif opcao == "3":
+        nome = input("Nome do item a remover: ").lower()
+
+        if nome in estoque:
+            del estoque[nome]
+            print(f"✔ Item '{nome}' removido!\n")
+        else:
+            print("❌ Item não encontrado.\n")
+
+    # ----------------- ATUALIZAR QUANTIDADE -----------------
+    elif opcao == "4":
+        nome = input("Nome do item: ").lower()
+
+        if nome in estoque:
+            nova_qtd = int(input("Nova quantidade: "))
+            estoque[nome]["quantidade"] = nova_qtd
+            print(f"✔ Quantidade de '{nome}' atualizada!\n")
+        else:
+            print("❌ Item não existe no estoque.\n")
+
+    # ----------------- LISTAR ESTOQUE -----------------
+    elif opcao == "5":
+        print("\n===== ESTOQUE ATUAL =====")
+
+        if len(estoque) == 0:
+            print("Estoque vazio.")
+        else:
+            for item, dados in estoque.items():
+                print(f"- {item}: {dados['quantidade']} unidades | R$ {dados['preco']:.2f}")
+
+        print()
+
+    # ----------------- SAIR -----------------
+    elif opcao == "6":
+        print("Saindo do sistema... Até mais!")
+        break
+
+    else:
+        print("❌ Opção inválida! Tente novamente.\n")
